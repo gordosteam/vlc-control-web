a20213207955b9a7d861912701ea436a5aeacc8a
Eduardo Malherbi <emalherbi@gmail.com>
2014-12-02 00:03:30 -0200

    .

--- app/js/app.js
+++ app/js/app.js
@@ -1,19 +1,61 @@
 /* app js */
+var VLCServer = {
 
-var VCL = {
-  play : function() {
+  url : "http://192.168.1.7:8124/",
 
-    
+  request : function( call ) {
+    NProgress.start();
 
+    $.ajax({
+      url: this.url,
+      dataType: "jsonp",
+      jsonpCallback: call,
+      cache: false,
+      timeout: 5000,
+      success: function(data) {
+        NProgress.done();
 
+        var r = jQuery.parseJSON( data );
+        $('#log').html( r.msg );
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        NProgress.done();
+
+        alert('error ' + textStatus + " " + errorThrown);
+      }
+    });
   }
 }
 
-// Shorthand for $( document ).ready()
+var VLC = {
+  backward : function() {
+    VLCServer.request( 'backward' );
+  },
+  play : function() {
+    VLCServer.request( 'play' );
+  },
+  stop : function() {
+    VLCServer.request( 'stop' );
+  },
+  forward : function() {
+    VLCServer.request( 'forward' );
+  }
+}
+
 $(function() {
   console.log( "ready!" );
 
-
-
+  $('#backward').click(function() {
+    VLC.backward();
+  });
+  $('#play').click(function() {
+    VLC.play();
+  });
+  $('#stop').click(function() {
+    VLC.stop();
+  });
+  $('#forward').click(function() {
+    VLC.forward();
+  });
 
 });
