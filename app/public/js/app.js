b16d548300b10c887e530b272416d57c31b95a39
Fernando Derkoski <brilvio@gmail.com>
2016-03-31 20:28:08 -0300

    Alterado para que funcione com api.ai para reconhecimento de comando de voz. Implementar o resto dos comandos.

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -5,13 +5,13 @@ var socketIO = {
 
   create : function() {
 
-    this.socket = io('192.168.1.4:3500');
+    this.socket = io('10.0.0.100:3500');
 
 	  var that = this.socket;
     this.socket.on('conectado', function(msg) {
       that.emit('cfg', {
-        ip : '192.168.1.4',
-        port : 8080,
+        ip : 'localhost',
+        port : 8000,
         user : '',
         password : 'asd123',
         pathImgs: 'vlc-control-web/app/public/imgs/' // 'c:\\xampp\\htdocs\\vlc-control-web\\app\\public\\imgs\\'
@@ -46,6 +46,92 @@ var socketIO = {
 
 $(function() {
   console.log( "ready!" );
+  
+        var accessToken = "";
+		var subscriptionKey = "";
+		var baseUrl = "https://api.api.ai/v1/";
+		$(document).ready(function() {
+			$("#input").keypress(function(event) {
+				if (event.which == 13) {
+					event.preventDefault();
+					send();
+				}
+			});
+			$("#rec").click(function(event) {
+				switchRecognition();
+			});
+		});
+		var recognition;
+		function startRecognition() {
+			recognition = new webkitSpeechRecognition();
+			recognition.onstart = function(event) {
+				updateRec();
+			};
+			recognition.onresult = function(event) {
+				var text = "";
+			    for (var i = event.resultIndex; i < event.results.length; ++i) {
+			    	text += event.results[i][0].transcript;
+			    }
+			    setInput(text);
+				stopRecognition();
+			};
+			recognition.onend = function() {
+				stopRecognition();
+			};
+			recognition.lang = "pt-BR";
+			recognition.start();
+		}
+	
+		function stopRecognition() {
+			if (recognition) {
+				recognition.stop();
+				recognition = null;
+			}
+			updateRec();
+		}
+		function switchRecognition() {
+			if (recognition) {
+				stopRecognition();
+			} else {
+				startRecognition();
+			}
+		}
+		function setInput(text) {
+			$("#input").val(text);
+			send();
+		}
+		function updateRec() {
+			$("#rec").text(recognition ? "Stop" : "Speak");
+		}
+		function send() {
+			var text = $("#input").val();
+			$.ajax({
+				type: "POST",
+				url: baseUrl + "query/",
+				contentType: "application/json; charset=utf-8",
+				dataType: "json",
+				headers: {
+					"Authorization": "Bearer " + accessToken,
+					"ocp-apim-subscription-key": subscriptionKey
+				},
+				data: JSON.stringify({ q: text, lang: "pt-br" }),
+				success: function(data) {
+					setResponse(JSON.stringify(data, undefined, 2));
+				},
+				error: function() {
+					setResponse("Internal Server Error");
+				}
+			});
+			setResponse("Loading...");
+		}
+		function setResponse(val) {
+			var obj = JSON.parse(val);
+			if (obj.result.action = 'playMusic') {
+				 socketIO.command( 'play', {
+                    novideo : true
+                  });
+			}
+		}
 
   $('#slider').slider({
     formatter: function(value) {
